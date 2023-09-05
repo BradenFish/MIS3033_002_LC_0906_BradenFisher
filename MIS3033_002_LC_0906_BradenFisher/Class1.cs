7b7b2a2fdcc393bcfdee7ab5b434820fe0c5fa3a
Braden Fisher <Braden.J.Fisher-1@ou.edu>
2023-09-05 12:41:51 -0500

    Person class added to branch 1

---
+++ MIS3033_002_LC_0906_BradenFisher/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace a
+{
+    public class Person
+    {
+        public string name {  get; set; }   
+        public DateTime dob { get; set; }
+        public double wieght { get; set; }
+        public Person() { } // empty constructor function
+        public Person (string name, DateTime dob, double wieght) 
+        { 
+            this.name = name;
+            this.dob = dob;
+            this.wieght = wieght;
+        }
+
+    }
+}
--- MIS3033_002_LC_0906_BradenFisher/Program.cs
+++ MIS3033_002_LC_0906_BradenFisher/Program.cs
@@ -2,5 +2,15 @@
 // September 6, 2023
 // Braden Fisher 113492081
 
+using a;
+
 Console.WriteLine("Interface");// class, interface
 
+Person p1;
+
+p1 = new Person();
+p1.name = "Braden Fisher";
+p1.dob = new DateTime(2001, 12, 21);
+p1.wieght = 189.4;// in lbs
+// this is on branch 1
+Console.WriteLine(p1.dob.ToString("MMMM dd, yyyy"));
