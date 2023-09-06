5fb28b3a05fe099f53d03cb3509f863ef9153389
Braden Fisher <Braden.J.Fisher-1@ou.edu>
2023-09-06 11:42:33 -0500

    Lecture 09/06/2023

---
+++ MIS3033_002_LC_0906_BradenFisher/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace a
+{
+    public class Person// Object
+    {
+        public double grade { get; set; }// C# Syntax// you need to know
+        public string name {  get; set; }// member field   
+        public DateTime dob { get; set; }
+        public double wieght { get; set; }
+        public virtual string CoffeeMaker() // virtual, replaceable
+        {
+
+            return "This is the coffee from person";
+        } 
+
+        public Person() { } // empty constructor function 1
+
+        // local variable
+        public Person (string name, DateTime dob, double wieght)// constructor function 2
+        {
+            Console.WriteLine(name);// local has priority
+            this.name = name;// this self 
+            this.dob = dob;
+            this.wieght = wieght;
+        }
+        // two functions have exactly the same name but different parameters. (Function overload)
+
+    }
+}
--- MIS3033_002_LC_0906_BradenFisher/Program.cs
+++ MIS3033_002_LC_0906_BradenFisher/Program.cs
@@ -3,14 +3,58 @@
 // Braden Fisher 113492081
 
 using a;
+using f1;
 
 Console.WriteLine("Interface");// class, interface
 
-Person p1;
+a.Person p1;// Person, blueprint, complex
 
-p1 = new Person();
+p1 = new Person();// function call, constructor function
+
+Person p2 = new Person("Braden", new DateTime(2000,6,29), 136);
+p1.grade = 96.6;// simple// write the value
+Console.WriteLine(p1.grade);// read the value
+//
+
 p1.name = "Braden Fisher";
 p1.dob = new DateTime(2001, 12, 21);
 p1.wieght = 189.4;// in lbs
 // this is on branch 1
 Console.WriteLine(p1.dob.ToString("MMMM dd, yyyy"));
+
+Student stu1;// student, complex
+stu1 = new Student();
+
+Console.WriteLine(stu1.CoffeeMaker());
+Console.WriteLine(stu1);
+Console.WriteLine(stu1.ToString());
+
+// simple: int double bool char
+// complex: string, student
+int age2; // int
+string str2;// string
+// var
+var age3 = 19;// var here is int
+//var age4; //
+var stu3 = new Student();// student datatype
+var stu4 = new Person();// 
+
+// namespace
+// folder concept
+namespace f1 // 2folder concept, 
+{
+    public class Student : Person // none. Student, complex
+    {
+        public string id { get; set; }
+        public Student() { }
+
+        public override string ToString()
+        {
+            return "This is a student";
+        }
+        public override string CoffeeMaker()
+        {
+            return "This is the coffee from Student";
+        }
+    }
+}
